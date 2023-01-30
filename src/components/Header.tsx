e762692b25590727d855faa63fae432c4c711b4d
Angelo Filho <angelo.filho06@hotmail.com>
2023-01-30 14:35:07 -0300

    chore: setup phosphor icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import logo from "../assets/Logo.svg";
+
 export function Header() {
   return (
-    <header className="">
-      <img src="" alt="" />
+    <header className="flex items-center justify-between">
+      <img src={logo} alt="Logo Coffe Delivery" />
 
-      <div>
+      <div className="flex items-center">
         <div>Porto Alegre</div>
 
         <button type="button" className="">
